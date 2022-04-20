f18aa93f72e8ba26a22d46b17a804d3c0244c22b
dwesslin <david.wessling@tu-dortmund.de>
2022-04-20 11:10:23 +0200

    add: generate Cities in GameConstructor

--- src/main/java/de/materna/alchemistpeddler/gamelogic/Game.java
+++ src/main/java/de/materna/alchemistpeddler/gamelogic/Game.java
@@ -1,8 +1,26 @@
 package de.materna.alchemistpeddler.gamelogic;
 
+import java.util.Collection;
+
 /**
  * Class that will handle communication between all GameObjects
  */
 public class Game {
-
+  private enum CITY_NAMES {
+    GOLDSHIRE("Goldshire"),
+    HOBBINGEN("Hobbingen"),
+    TRISTRAM("Tristram"),
+    CAMELOT("Camelot"),
+    ATLANTIS("Atlantis");
+    private final String cityName;
+    CITY_NAMES(String cityName) {
+      this.cityName = cityName;
+    }
+  }
+  private City[] cities = new City[CITY_NAMES.values().length];
+  public Game(){
+    for (CITY_NAMES value : CITY_NAMES.values()) {
+      cities[value.ordinal()] = new City(value.cityName);
+    }
+  }
 }
