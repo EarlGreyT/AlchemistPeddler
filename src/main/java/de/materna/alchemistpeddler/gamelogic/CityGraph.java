b5a0e1d79584d20450945813b7cc679e92f377e6
dwesslin <david.wessling@tu-dortmund.de>
2022-04-26 13:38:19 +0200

    fix: traveling from A to B will cost the same amount as traveling from B to A

--- src/main/java/de/materna/alchemistpeddler/gamelogic/CityGraph.java
+++ src/main/java/de/materna/alchemistpeddler/gamelogic/CityGraph.java
@@ -6,26 +6,35 @@ import java.util.EnumMap;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
- * Builds a fully connected Graph of Cities with random travel costs from each city to each other city
+ * Builds a fully connected Graph of Cities with random travel costs from each city to each other
+ * city
  */
 public final class CityGraph {
 
-  private  EnumMap<CITY_NAME, EnumMap<CITY_NAME, Route>> routes  = new EnumMap<>(CITY_NAME.class);
-  public  void buildGraph(){
-    for (CITY_NAME from: CITY_NAME.values()){
+  private EnumMap<CITY_NAME, EnumMap<CITY_NAME, Route>> routes = new EnumMap<>(CITY_NAME.class);
+
+  public void buildGraph() {
+    for (CITY_NAME from : CITY_NAME.values()) {
       EnumMap<CITY_NAME, Route> destMap = new EnumMap<>(CITY_NAME.class);
-      for (CITY_NAME dest: CITY_NAME.values()){
-        if (from.name().equals(dest.name())){
-          destMap.put(dest,new Route(from,dest,0));
-        } else{
-          int randomPrice = ThreadLocalRandom.current().nextInt(50,301);
-          destMap.put(dest, new Route(from,dest,randomPrice));
+      for (CITY_NAME dest : CITY_NAME.values()) {
+        if (from.name().equals(dest.name())) {
+          destMap.put(dest, new Route(from, dest, 0));
+        } else {
+          int price = 0;
+          try {
+            price = getPrice(dest, from);
+          } catch (NullPointerException e) {
+            price = ThreadLocalRandom.current().nextInt(50, 301);
+          } finally {
+            destMap.put(dest, new Route(from, dest, price));
+          }
         }
       }
       routes.put(from, destMap);
     }
   }
-  public int getPrice(CITY_NAME from, CITY_NAME dest){
+
+  public int getPrice(CITY_NAME from, CITY_NAME dest) {
     return routes.get(from).get(dest).cost();
   }
 }
